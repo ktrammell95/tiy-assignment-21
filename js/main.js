6efb9cff5d669327a08dd2f28e67896c95cf6064
Katherine Trammell <ktrammell@gmail.com>
2015-03-03 14:29:34 -0500

    started setting up file to pull in one contact

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 var Contact = Backbone.Model.extend({
-  url: "js/contact.json"
+  url: "js/data.json"
 });
 
 var ContactView = (function() {
-  var template = JST["contacts"];
+  var template = JST["contactsRow"];
 
   function ContactView(model) {
-    _.extend(this,)
+    this.model = model;
+    this.$el = $("<table />");
   }
+
+  ContactView.prototype = {
+    render: function() {
+      var markup = template( this.model.toJSON() )
+      return this.$el.html( markup );
+    }
+  }
+
+  return ContactView;
+
+})();
+
+$(function() {
+  var contactModel = new Contact();
+
+  var contactView = new ContactView(contactModel);
+
+  contactModel.fetch().done(function() {
+    $("body").append(contactView.render());
+
+  });
+
 });
