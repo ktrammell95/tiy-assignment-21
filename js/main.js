88f580f9e5f4553552755f2751960e9a285f9bcf
Katherine Trammell <ktrammell@gmail.com>
2015-03-03 13:44:21 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var Contact = Backbone.Model.extend({
+  url: "js/contact.json"
+});
+
+var ContactView = (function() {
+  var template = JST["contacts"];
+
+  function ContactView(model) {
+    _.extend(this,)
+  }
+});
