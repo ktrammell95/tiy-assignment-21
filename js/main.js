07b766a54aa363a9312ca38aab293e263deb13a0
Katherine Trammell <ktrammell@gmail.com>
2015-03-03 18:32:41 -0500

    minor styling tweaks

--- js/main.js
+++ js/main.js
@@ -1,34 +1,75 @@
-var Contact = Backbone.Model.extend({
-  url: "js/data.json"
+var Employee = Backbone.Model.extend({
+  // url: "js/data.json"
 });
 
-var ContactView = (function() {
+//table rows for contact information
+
+var EmployeeView = (function() {
   var template = JST["contactsRow"];
 
-  function ContactView(model) {
+  function EmployeeView(model) {
+    _.extend(this, Backbone.Events)
     this.model = model;
-    this.$el = $("<table />");
+    this.$el = $("<tr />");
+    this.$el.addClass("tableRow");
+    this.listenTo(this.model, "change", function() {
+      this.render();
+    });
   }
 
-  ContactView.prototype = {
+  EmployeeView.prototype = {
     render: function() {
       var markup = template( this.model.toJSON() )
       return this.$el.html( markup );
     }
   }
 
-  return ContactView;
+  return EmployeeView;
 
 })();
 
+//table headers for columns
+
+var HeaderView = (function() {
+  var template = JST["contactsHead"];
+
+  function HeaderView(headings) {      
+    _.extend(this, Backbone.Events);
+      this.headings = headings;
+      this.$el = $("<tr />");
+      this.$el.addClass("tableHead"); 
+    }
+
+  HeaderView.prototype = {
+    render:function() {
+      // map headings into objects that look like { name: "Name"}
+      var data = _.map(this.headings, function(heading){
+          return {name: heading};
+      });
+      // loop through them
+      _.each(data, function(datum){
+        // append th template to tr (this.$el)
+        this.$el.append(template(datum)) 
+      }, this);
+      return this.$el;
+    }
+  }
+  return HeaderView;
+
+})();
+
+//ajax function
+
 $(function() {
-  var contactModel = new Contact();
+  $.ajax("js/data.json").done(function(data){
+    var headings = Object.keys(data[0]);
+    var headView = new HeaderView(headings);
+    $("#contacts thead").html(headView.render());
 
-  var contactView = new ContactView(contactModel);
-
-  contactModel.fetch().done(function() {
-    $("body").append(contactView.render());
-
+    _.each(data, function(datum){
+      var employeeModel = new Employee(datum);
+      var employeeView = new EmployeeView(employeeModel);
+      $("#contacts tbody").append(employeeView.render());
+    });
   });
-
 });
--- js/templates.js
+++ js/templates.js
@@ -1,19 +1,22 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["contactsHead"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "<table class = \"tableHead\">\n  <tr>\n    <th class=\"headerName\">Name</th>\n    <th class=\"headerRole\">Role</th>\n    <th class=\"headerManager\">Manager</th>\n    <th class=\"headerPhone\">Phone</th>\n    <th class=\"headerDept\">Dept</th>\n  </tr>\n</table>\n";
-  },"useData":true});
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "  <th>"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</th>\n";
+},"useData":true});
 this["JST"] = this["JST"] || {};
 this["JST"]["contactsRow"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return " <table class = \"tableBody\"> \n  <tr>\n    <td class=\"name\">"
+  return "  <td class=\"name\">"
     + escapeExpression(((helper = (helper = helpers.Name || (depth0 != null ? depth0.Name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Name","hash":{},"data":data}) : helper)))
-    + "</td>\n    <td class=\"role\">"
+    + "</td>\n  <td class=\"role\">"
     + escapeExpression(((helper = (helper = helpers.Role || (depth0 != null ? depth0.Role : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Role","hash":{},"data":data}) : helper)))
-    + "</td>\n    <td class=\"manager\">"
+    + "</td>\n  <td class=\"manager\">"
     + escapeExpression(((helper = (helper = helpers.Manager || (depth0 != null ? depth0.Manager : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Manager","hash":{},"data":data}) : helper)))
-    + "</td>\n    <td class=\"phone\">"
+    + "</td>\n  <td class=\"phone\">"
     + escapeExpression(((helper = (helper = helpers.Phone || (depth0 != null ? depth0.Phone : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Phone","hash":{},"data":data}) : helper)))
-    + "</td>\n    <td class=\"dept\">"
+    + "</td>\n  <td class=\"dept\">"
     + escapeExpression(((helper = (helper = helpers.Dept || (depth0 != null ? depth0.Dept : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Dept","hash":{},"data":data}) : helper)))
-    + "</td>\n  </tr>\n</table>";
+    + "</td>\n";
 },"useData":true});
