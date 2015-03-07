2ddc6fa979f49a822adbbaa228781d34a583b705
Katherine Trammell <ktrammell@gmail.com>
2015-03-07 09:55:56 -0500

    played with CSS

--- js/templates.js
+++ js/templates.js
@@ -17,7 +17,9 @@ this["JST"]["contactsHead"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.
 this["JST"] = this["JST"] || {};
 this["JST"]["contactsRow"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "  <td class=\"name\">"
+  return "  <td class=\"photo\"><img src="
+    + escapeExpression(((helper = (helper = helpers.Photo || (depth0 != null ? depth0.Photo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Photo","hash":{},"data":data}) : helper)))
+    + "></td>\n  <td class=\"name\">"
     + escapeExpression(((helper = (helper = helpers.Name || (depth0 != null ? depth0.Name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Name","hash":{},"data":data}) : helper)))
     + "</td>\n  <td class=\"role\">"
     + escapeExpression(((helper = (helper = helpers.Role || (depth0 != null ? depth0.Role : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Role","hash":{},"data":data}) : helper)))
@@ -31,7 +33,5 @@ this["JST"]["contactsRow"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     + escapeExpression(((helper = (helper = helpers.Start || (depth0 != null ? depth0.Start : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Start","hash":{},"data":data}) : helper)))
     + "\">"
     + escapeExpression(((helper = (helper = helpers.Start || (depth0 != null ? depth0.Start : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Start","hash":{},"data":data}) : helper)))
-    + "</time></td>\n  <td>"
-    + escapeExpression(((helper = (helper = helpers.Icon || (depth0 != null ? depth0.Icon : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"Icon","hash":{},"data":data}) : helper)))
-    + "</td>\n";
+    + "</time></td>\n";
 },"useData":true});
