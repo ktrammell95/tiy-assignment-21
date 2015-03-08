5e730a4050d69c952c5043e890bf194908615260
Katherine Trammell <ktrammell@gmail.com>
2015-03-08 10:30:36 -0400

    attempting custom checkboxes

--- js/templates.js
+++ js/templates.js
@@ -1,11 +1,11 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["checkbox"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "  <input type=\"checkbox\" value=\"None\" class=\"dept\" name=\""
+  return "\n<!-- Rounded ONE -->\n<div class=\"roundedOne\">\n  <input type=\"checkbox\" value=\"None\" id=\"roundedOne\" name=\""
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "\" / >\n  <label class=\"roundedOne\">"
+    + "\" />\n  <label for=\"roundedOne\">"
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "</label>\n\n<!-- Rounded ONE -->\n";
+    + "</label>\n</div>";
 },"useData":true});
 this["JST"] = this["JST"] || {};
 this["JST"]["contactsHead"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
